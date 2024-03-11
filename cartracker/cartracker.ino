225e2ccc23774fabf7db7810627c513c2a59f433
Rui Romao <github@ruiromao.pt>
2024-03-11 16:03:31 +0000

    modified:   .gitignore 	modified:   cartracker/cartracker.ino

--- cartracker/cartracker.ino
+++ cartracker/cartracker.ino
@@ -1,8 +1,80 @@
-#include "utilities.h"
+/**
+ * @file      cartracker.ino
+ * @author    Rui Romao
+ * @license   MIT
+ * @copyright Copyright (c) 2024
+ * @date      2024-03-11
+ * @note      
+ */
 
+#include "utilities.h"
+#include <TinyGsmClient.h>
+
+// Define the serial console for debug prints, if needed
+#define TINY_GSM_DEBUG SerialMon
+
+// Set serial for debug console (to the Serial Monitor, default speed 115200)
+#define SerialMon Serial
+
+// See all AT commands, if wanted
+#define DUMP_AT_COMMANDS
+
+#define SMS_TARGET  "+380xxxxxxxxx" //Change the number you want to send sms message
+
+#ifdef DUMP_AT_COMMANDS  // if enabled it requires the streamDebugger lib
+#include <StreamDebugger.h>
+StreamDebugger debugger(SerialAT, SerialMon);
+TinyGsm modem(debugger);
+#else
+TinyGsm modem(SerialAT);
+#endif
 
 void setup()
 {
+    Serial.begin(115200);
+    // Turn on DC boost to power on the modem
+    #ifdef BOARD_POWERON_PIN
+        pinMode(BOARD_POWERON_PIN, OUTPUT);
+        digitalWrite(BOARD_POWERON_PIN, HIGH);
+    #endif
+
+    // Set modem reset pin ,reset modem
+    pinMode(MODEM_RESET_PIN, OUTPUT);
+    digitalWrite(MODEM_RESET_PIN, !MODEM_RESET_LEVEL); delay(100);
+    digitalWrite(MODEM_RESET_PIN, MODEM_RESET_LEVEL); delay(2600);
+    digitalWrite(MODEM_RESET_PIN, !MODEM_RESET_LEVEL);
+
+    // Set ring pin input
+    pinMode(MODEM_RING_PIN, INPUT_PULLUP);
+
+    // Set modem baud
+    SerialAT.begin(115200, SERIAL_8N1, MODEM_RX_PIN, MODEM_TX_PIN);
+
+    Serial.println("Start modem...");
+    delay(3000);
+
+    int retry = 0;
+    while (!modem.testAT(1000)) {
+        Serial.println(".");
+        if (retry++ > 10) {
+            digitalWrite(BOARD_PWRKEY_PIN, LOW);
+            delay(100);
+            digitalWrite(BOARD_PWRKEY_PIN, HIGH);
+            delay(1000);
+            digitalWrite(BOARD_PWRKEY_PIN, LOW);
+            retry = 0;
+        }
+    }
+    Serial.println();
+    delay(200);
+    Serial.print("GSM Modem init success, moving onto GPS init...");
+
+    while (!modem.enableGPS(MODEM_GPS_ENABLE_GPIO)) {
+        Serial.print(".");
+    }
+    Serial.println();
+    Serial.println("GPS Enabled");
+
 }
 
 void loop()
